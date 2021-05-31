14353d324bc66164c7cd1a8ecb408b94049b4e1c
sub722 <jhswebdeb@gmail.com>
2021-06-01 00:23:10 +0200

    force dark theme

--- src/ThemeContext.tsx
+++ src/ThemeContext.tsx
@@ -26,7 +26,7 @@ const ThemeContextProvider: React.FC = ({ children }) => {
 
   return (
     <ThemeContext.Provider value={{ isDark, toggleTheme }}>
-      <SCThemeProvider theme={isDark ? dark : light}>{children}</SCThemeProvider>
+      <SCThemeProvider theme={dark}>{children}</SCThemeProvider>
     </ThemeContext.Provider>
   )
 }
