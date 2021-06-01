aaa03d524a4a00a218dada17ea7d7b6a13cef976
sub722 <jhswebdeb@gmail.com>
2021-06-01 18:48:58 +0200

    edited transparency

--- src/pages/AppBody.tsx
+++ src/pages/AppBody.tsx
@@ -15,8 +15,8 @@ export const BodyBG = styled(Card)`
   width: 100%;
   height:100%;
   
-  background:rgba(0,0,0,.5);
-  backdrop-filter:blur(20px);
+  background:rgba(0,0,0,.2);
+  backdrop-filter:blur(10px);
 `
 
 
